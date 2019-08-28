dd588c687ad76fc11cb3ff6f8722beb041efd3a2
careteenL <15074806497@163.com>
2019-08-28 10:00:11 +0800

    leetcode: update 038-count-and-say

--- src/leetcode/038-count-and-say.js
+++ src/leetcode/038-count-and-say.js
@@ -1,5 +1,5 @@
 /**
- * @desc 报数
+ * @desc 报数 方法一
  * @leetcode https://leetcode-cn.com/problems/count-and-say/
  * @param {number} n
  * @return {string}
@@ -27,3 +27,30 @@ console.log(countAndSay(1))
 console.log(countAndSay(2))
 console.log(countAndSay(3))
 console.log(countAndSay(4))
+
+/**
+ * @desc 方法二 递归实现
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay2 = function (n) {
+  if (n === 1) return '1'
+  let pre = countAndSay2(n - 1)
+  let temp = ''
+  for (let i = 0, j = 0; i < pre.length;) {
+    if (pre[i] === pre[i + j]) {
+      j++
+    } else {
+      temp = temp + j + '' + pre[i]
+      i += j
+      j = 1
+    }
+  }
+  return temp
+}
+
+// Test
+console.log(countAndSay2(1))
+console.log(countAndSay2(2))
+console.log(countAndSay2(3))
+console.log(countAndSay2(4))
